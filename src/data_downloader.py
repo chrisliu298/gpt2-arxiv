5206c03e72935e4fdcd5128cc6ff2856f4c5a59d
yuhaoliu298 <yuhaoliu298@gmail.com>
2020-04-08 10:13:05 -0700

    Modifying var names

--- src/data_downloader.py
+++ src/data_downloader.py
@@ -1,4 +1,6 @@
 from utils.download_reddit_data import DataPipeline
 
-reddit_downloader = DataPipeline("nosleep", "submission", start=1577836800)
+reddit_downloader = DataPipeline(
+    "nosleep", "submission", start=1554768000, least_num_comments=3
+)
 reddit_downloader.download()
--- src/utils/download_reddit_data.py
+++ src/utils/download_reddit_data.py
@@ -1,24 +1,26 @@
+from datetime import datetime
 import requests
 import json
-import re
-import time
 
 
 class DataPipeline:
-    def __init__(self, subreddit, option="submission", start=0):
+    def __init__(self, subreddit, option="submission", start=0, least_num_comments=3):
         self.subreddit = subreddit
         self.option = option
         self.start = start
         self.data = []
+        self.least_num_comments = least_num_comments
         self.url = "http://api.pushshift.io/reddit"
 
     def fetch(self, **kwargs):
         params = {"sort_type": "created_utc", "sort": "asc", "size": 1000}
-        params['subreddit'] = self.subreddit
-        params['option'] = self.option
-        params['after'] = kwargs['after']
+        params["subreddit"] = self.subreddit
+        params["option"] = self.option
+        params["after"] = kwargs["after"]
         print(params)
-        r = requests.get(self.url + "/" + self.option + "/search/", params=params, timeout=30)
+        r = requests.get(
+            self.url + "/" + self.option + "/search/", params=params, timeout=30
+        )
         if r.status_code == 200:
             response = json.loads(r.text)
             data = response["data"]
@@ -30,6 +32,9 @@ class DataPipeline:
         max_id = 0
         filename = self.subreddit
         file = open(f"{filename}.txt", "w+")
+        count = 0
+        start_date = datetime.fromtimestamp(self.start).strftime("%Y-%m-%d %H:%M:%S")
+        end_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         while 1:
             nothing_processed = True
             objects = self.fetch(**kwargs, after=max_created_utc)
@@ -43,16 +48,24 @@ class DataPipeline:
                         max_created_utc = created_utc
                     try:
                         text = object["selftext"]
-                        if text == "[removed]" or text == "[deleted]" or text == "":
+                        num_comments = object["num_comments"]
+                        if (
+                            text == "[removed]"
+                            or text == "[deleted]"
+                            or text == ""
+                            or num_comments < self.least_num_comments
+                        ):
                             continue
                         else:
-                            file.write("<|startoftext|> " + text + " <|endoftext|>\n")
+                            count += 1
+                            file.write(f"<|startoftext|> " + text + " <|endoftext|>\n")
                             # self.data.append(text + " <|endoftext|>")
                     except:
                         continue
             if nothing_processed:
+                file.close()
+                print(
+                    f"{count} stories from {start_date} to {end_date} with at least {self.least_num_comments} comments were fetched."
+                )
                 return
             max_created_utc -= 1
-        file.close()
-
-    
